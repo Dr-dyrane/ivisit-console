f1fff458fbb1f739de983a5bb7a14e5d987cad67
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2026-02-03 22:22:01 -0800

    modified:   frontend/src/components/navigation/SmartHeader.jsx

--- frontend/src/components/navigation/SmartHeader.jsx
+++ frontend/src/components/navigation/SmartHeader.jsx
@@ -35,7 +35,9 @@ export const SmartHeader = () => {
                         : 'top-0 left-0 right-0'
                     }`}
                 style={{
-                    paddingRight: isMobile ? '16px' : '32px'
+                    paddingRight: isMobile ? '16px' : '32px',
+                    paddingTop: 'var(--safe-top)',
+                    height: 'calc(2rem + var(--safe-top))'
                 }}
             >            {/* Simple Static Dot Grid - Apple-level simplicity */}
 
